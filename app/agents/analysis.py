10b1d6529e17bb72e632444bf9efb293493bf64c
Debojit Sinha <debojit.sinha@oracle.com>
2025-10-30 19:32:29 +0000

    Modified tool response format in prompt analysis agent.

--- app/agents/analysis.py
+++ app/agents/analysis.py
@@ -9,18 +9,19 @@ from app.models.agent import PromptAnalysis
 ANALYSER_PROMPT = (
     "You are a prompt analysis assistant."
     "Analyse the user input and extract the country name and year from it."
-    "If needed, convert the country name to the ISO-3166-2 country code."
-    "Input year is to be converted to YYYY format if needed."
-    "If input year is missing in the user prompt, check current date & time. If current month is after 'June', then return current year in YYYY format, else return previous year in YYYY format."
+    "Convert the country name to the ISO-3166-2 country code if needed."
+    "If year is missing in the user prompt, check current date. If current month is after 'June', then return current year in YYYY format, else return the previous year in YYYY format."
+    "Convert the year to YYYY format if needed."
 )
 
-@tool("current_datetime", description="Use this tool to get the current date & time.")
-def get_current_datetime() -> datetime:
+@tool("current_datetime",
+      description="Use this tool to get the current date in YYYY-MM-DD format.")
+def get_current_date() -> str:
     """Returns current date & time."""
-    return datetime.now()
+    return datetime.now().strftime("%Y-%m-%d")
 
 analyser = create_agent(name="Prompt Analyser",
                         model=get_llm(),
-                        tools=[get_current_datetime],
+                        tools=[get_current_date],
                         response_format=PromptAnalysis,
                         system_prompt=ANALYSER_PROMPT)
