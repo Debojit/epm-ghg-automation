f4b4e035bedcb0e0ccf2efbe62012f0996e1efbe
Debojit Sinha <debojit.sinha@oracle.com>
2025-10-31 03:54:06 +0000

    Additional test cases for supervisor agent.

--- tests/agents/test_supervisor.py
+++ tests/agents/test_supervisor.py
@@ -15,6 +15,38 @@ def test_valid_input():
                 }
             ]
         })
-    import pprint
-    pprint.pp(result["messages"][-1].content)
     assert result["messages"][-1] not in (None, "")
+
+def test_invalid_country():
+    """
+    Test if the agent workflow is performing normally with valid inputs.
+
+    Inputs: Country Code: GB, Year: 2025
+    Expected: Conversion factors download link.
+    """
+    result = supervisor.invoke({
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Please find GHG conversion factors for the Wakanda in 2025"
+                }
+            ]
+        })
+    assert result["messages"][-1].content == "Invalid Inputs."
+
+def test_invalid_year():
+    """
+    Test if the agent workflow is performing normally with valid inputs.
+
+    Inputs: Country Code: GB, Year: 2025
+    Expected: Conversion factors download link.
+    """
+    result = supervisor.invoke({
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Please find GHG conversion factors for the United Kingdom in 1800"
+                }
+            ]
+        })
+    assert result["messages"][-1].content == "Invalid Inputs."
